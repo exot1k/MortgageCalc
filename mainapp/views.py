90fe88d973fa6341b105522c2a0065d56f5edb5c
Vikto <sashakaterinochkin@mail.ru>
2021-12-05 16:47:07 +0300

    add comment

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,16 +6,20 @@ from rest_framework.response import Response
 
 class MortgageViewSet(views.APIView):
 
+    # Аннуитетный метод
     @staticmethod
     def get_annuity_mortgage(sum, period, rate):
+
         month_rate = rate / 12
         top_part = (sum * month_rate)
         bottom_part = (1 - (1 / (month_rate + 1) ** period))
         ann = (top_part / bottom_part)
         return Response(ann)
 
+    # Дифференцированный метод
     @staticmethod
     def get_differentiated_mortgage(sum, period, rate):
+
         remain_summa = sum
         month_rate = rate / 12
         main_debt = +(sum / period)
@@ -34,11 +38,14 @@ class MortgageViewSet(views.APIView):
             })
         return Response(response)
 
+    # get метод
     def get(self, request, *args, **kwargs):
+        # Получение параметров body
         sum = int(self.request.data['sum'])
         period = int(self.request.data['period'])
         rate = float(self.request.data['rate'])
         is_diff = strtobool(self.request.data['is_diff'])
+        # Вызов метода в зависимости от типа рассчета
         if is_diff:
             return self.get_differentiated_mortgage(sum, period, rate)
         return self.get_annuity_mortgage(sum, period, rate)
